96b40415d8857583acda5ff9666ba73b11f54293
Fabian Rehhauße <f.rehhausse@klebe.io>
2020-12-17 19:17:29 +0100

    Add simple example

--- run/kubejs/startup_scripts/script.js
+++ run/kubejs/startup_scripts/script.js
@@ -1,15 +1,11 @@
 // priority: 0
 
 events.listen('owncrops.crop.registry', event => {
-    event.create("rice")
-            .edible(10, 3)
-            .plantable()
-            .addGrowthStage(2)
-            .addGrowthStage(16);
-    event.create("tomato")
-            .edible(10, 3);
-    event.create("tomato_seeds")
-            .plantable()
+    event.create("dark_crop")
+            .edible(2, 4)
+
+    event.create("dark_seed")
+            .plantable("owncrops:dark_crop")
             .addGrowthStage(2)
             .addGrowthStage(16);
 })
